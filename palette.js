b6e7e0f5fce722a50940331c79727366c1519906
marco <sunhq@dtdream.com>
2017-06-05 16:52:52 +0800

    impose polyline cache: draw polylines only if lines lies in map bounded area

--- palette.js
+++ palette.js
@@ -15,6 +15,7 @@ export default class Palette {
       case 'polyline':
         this.__proto__.importPolyline = Polyline.importPolyline;
         this.__proto__.draw = Polyline.draw;
+        this.__proto__.cacheVisiblePolyline = Polyline.cacheVisiblePolyline;
         break;
       default:
         throw new Error('Unknown type');
@@ -26,6 +27,14 @@ export default class Palette {
     this.canvas.width = this.map.getSize().width;
     this.canvas.height = this.map.getSize().height;
     this.ctx = this.canvas.getContext('2d');
+  };
+  
+  setBoundLngLat() {
+    let bounds, southwest, northeast;
+    bounds = this.map.getBounds();
+    southwest = bounds.getSouthWest();
+    northeast = bounds.getNorthEast();
+    this.boundLngLat = [southwest.lng, southwest.lat, northeast.lng, northeast.lat]
   };
   
   /*
--- polyline.js
+++ polyline.js
@@ -4,13 +4,18 @@
   *@return {Object}
 */
 function importPolyline(polyline) {
-  this.polyline = polyline;
+  this.polyline = {};
+  this.polyline.original = polyline;
 };
 
 function draw() {
+  //cache visible data
+  this.setBoundLngLat();
+  this.cacheVisiblePolyline();
+  
   this.ctx.strokeStyle = '#FF0000';
   this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
-  this.polyline.forEach((eachline) => {
+  this.polyline.cache.forEach((eachline) => {
     this.ctx.beginPath();
     eachline.forEach((point) => {
       let lnglat, clnglat;
@@ -22,7 +27,36 @@ function draw() {
   });
 };
 
+function cacheVisiblePolyline() {
+  let boundLngBuffer,
+      boundLatBuffer,
+      expandedBoundLngLat;
+  
+  boundLngBuffer = this.boundLngLat[2] - this.boundLngLat[0];
+  boundLatBuffer = this.boundLngLat[3] - this.boundLngLat[1];
+  expandedBoundLngLat = [this.boundLngLat[0] - boundLngBuffer, this.boundLngLat[1] - boundLatBuffer, this.boundLngLat[2] + boundLngBuffer, this.boundLngLat[3] + boundLatBuffer];
+  
+  this.polyline.cache = [];
+  
+  this.polyline.original.forEach((eachline) => {
+    let result = [];
+    
+    eachline.forEach((point) => {
+      let lng = point[0],
+          lat = point[1];
+      if (lng >= expandedBoundLngLat[0] && lat >= expandedBoundLngLat[1] && lng <= expandedBoundLngLat[2] && lat <= expandedBoundLngLat[3] ) {
+        result.push(point);
+      }
+    });
+    
+    if (result.length !== 0) {
+      this.polyline.cache.push(result);
+    }
+  });
+};
+
 export default {
   importPolyline,
   draw,
+  cacheVisiblePolyline,
 }
