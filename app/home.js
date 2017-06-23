81306f3d1517ce8634ca9ce5529855079c6ddf6d
marco <sunhq@dtdream.com>
2017-06-23 16:05:34 +0800

    delete empty line: for perfection :D

--- app/home.js
+++ app/home.js
@@ -25,7 +25,6 @@ export default class Home extends React.Component {
           <li><Link to="/phonePaySummary">bus phone pay summary 公交车无线支付统计</Link></li>
           <li><Link to="/originDestinationSummary">origin destination summary 交通量调查</Link></li>
           <li><Link to="/heatmap">heatmap 无线支付热力图</Link></li>
-          
         </ul>
       </div>
     );
