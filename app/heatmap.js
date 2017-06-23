34c85722b9b34f99bf367bd13a65b53e1ee804f8
marco <sunhq@dtdream.com>
2017-06-23 15:37:00 +0800

    refactor heatmap demo

--- app/heatmap.js
+++ app/heatmap.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import echarts from 'echarts';
 
 import Mavas from '../lib/mavas/main';
-import Util from '../lib/mavas/util';
 
-/*
-  *Map component creates a container for map
-  *container size is controlled by css styles
-  *initialise Amap as soon as Map component is mounted in react lifecycle
-*/
 export default class Heatmap extends React.Component {
   
   constructor(props) {
     super(props);
     this.props = props;
+    this.state = {
+      isFetching: true,
+    };
   };
   
   componentDidMount() {
@@ -22,29 +18,29 @@ export default class Heatmap extends React.Component {
       resizeEnable: true,
       zoom: 16,
       center: [120.131537, 30.281016],
-      animateEnable: false,
+      animateEnable: true,
     });
     
     //init amap layers on demand; see amap api reference
     this.mavas.map.plugin(['AMap.Heatmap'], () => {});
     
+    //TODO: STRONGLY recommand superagent in production
     var request = new XMLHttpRequest();
     request.open('POST', 'http://10.88.1.227:8080/getByStation', true);
     request.setRequestHeader('Content-type','application/x-www-form-urlencoded');
     request.send('startDate=2017-6-2&endDate=2017-6-22');
     request.onreadystatechange = () => {
       if (request.readyState === 4 && request.status === 200){
-        this.mockData = JSON.parse(request.response);
+        this.setState({
+          isFetching: false,
+        });
+        this.dataTransformation(JSON.parse(request.response))
         this.draw();
       };
     };
   };
   
   draw() {
-    let palette, polylinePalette, markerPalette, tooltipPalette;
-    
-    this.dataTransformation();
-    
     let heatmap = new AMap.Heatmap(this.mavas.map, {
       radius: 15, //给定半径
       opacity: [0, 0.8],
@@ -55,10 +51,10 @@ export default class Heatmap extends React.Component {
     });
   };
   
-  dataTransformation() {
+  dataTransformation(apiData) {
     this.data = [];
     
-    this.mockData.forEach((currentPoint) => {
+    apiData.forEach((currentPoint) => {
       this.data.push({
         lng: currentPoint.location.x,
         lat: currentPoint.location.y,
@@ -72,7 +68,8 @@ export default class Heatmap extends React.Component {
   render() {
     return (
       <div>
-        <h1>Heatmap</h1>
+        <h1>Heatmap 公交车站无限支付方式统计</h1>
+        <strong>请求API状态：<em>{this.state.isFetching ? '正在请求' : '请求成功'}</em></strong>
         <div className="map-container" id="map"></div>
       </div>
     );
