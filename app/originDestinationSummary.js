3e566878690acff978fed528e114e0cf6febc84f
marco <sunhq@dtdream.com>
2017-07-13 16:31:16 +0800

    make timeAxis configurable && add cover to timeAxis

--- app/originDestinationSummary.js
+++ app/originDestinationSummary.js
@@ -322,7 +322,9 @@ export default class OriginDestinationSummary extends React.Component {
         <h1>Origin Destination Summary</h1>
         <strong>请求API状态：<em>{this.state.isFetching ? '正在请求' : '请求成功'}</em></strong>
         <strong style={{'color': 'red', 'marginLeft': '30px'}}>点击气泡看看？</strong>
-        <canvas id="time-axis" style={{'width': '100%', 'padding': '20px 0'}}></canvas>
+        <div style={{'padding': '20px 0'}}>
+          <canvas id="time-axis" style={{'width': '100%'}}></canvas>
+        </div>
         <div className="map-container" id="map" style={{'height': 'calc(100vh - 200px)'}}></div>
       </div>
     );
