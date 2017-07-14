788bcd1808d8ac54db3b241172ca44d5378784c0
marco <sunhq@dtdream.com>
2017-07-14 11:10:15 +0800

    fetch recent 15 days data; assign heatmap radius for each zoom level;

--- app/heatmap.js
+++ app/heatmap.js
@@ -2,6 +2,7 @@ import React from 'react';
 import request from 'superagent';
 
 import Mavas from '../lib/mavas/main';
+import Util from '../lib/mavas/util';
 
 export default class Heatmap extends React.Component {
   
@@ -11,16 +12,45 @@ export default class Heatmap extends React.Component {
     this.state = {
       isFetching: true,
     };
+    this.baseHeatmapOptions = {
+      radius: 7, //给定半径
+      gradient: {
+//        '0.4': '#01579b',
+//        '0.7': '#0277bd',
+//        '0.8': '#0288d1',
+//        '1.0': 'white',
+        '0.4': '#4d69ff',
+        '0.7': '#4d73ff',
+        '0.8': '#6889ff',
+        '1.0': 'white',
+      },
+      opacity: [0.2, 0.8],
+    };
   };
   
   componentDidMount() {
+    this.startDate = new Date();
+    this.startDate.setDate(this.startDate.getDate() - 15);
+    this.endDate = new Date();
+    
     //init mavas; see amap api reference
     this.mavas = new Mavas('map',{
       resizeEnable: true,
-      zoom: 14,
+      zoom: 12,
       center: [120.16405,30.254651],
       animateEnable: true,
       mapStyle: 'amap://styles/darkblue',
+    });
+    
+    this.mavas.map.on('zoomchange', (e) => {
+      const zoomLevel = this.mavas.map.getZoom(),
+        radius = this.mapZoomToRadius(zoomLevel);
+      console.log(zoomLevel);
+      console.log(radius);
+      this.heatmap.setOptions({
+        ...this.baseHeatmapOptions,
+        radius: radius,
+      });
     });
     
     //init amap layers on demand; see amap api reference
@@ -28,8 +58,8 @@ export default class Heatmap extends React.Component {
     
     return request.get('http://10.85.1.171:8080/getByStation')
       .query({
-        startDate: '2017-6-2',
-        endDate: '2017-6-22',
+        startDate: `${this.startDate.getFullYear()}-${this.startDate.getMonth() + 1}-${this.startDate.getDate()}`,
+        endDate: `${this.endDate.getFullYear()}-${this.endDate.getMonth() + 1}-${this.endDate.getDate()}`,
       })
       .then((res) => {
         this.setState({
@@ -41,18 +71,14 @@ export default class Heatmap extends React.Component {
   };
   
   draw() {
-    let heatmap = new AMap.Heatmap(this.mavas.map, {
-      radius: 15, //给定半径
-      gradient: {
-        '.5': 'blue',
-        '.8': 'red',
-        '.95': 'white'
-      },
-      opacity: [0, 0.8],
-    });
-    heatmap.setDataSet({
+    this.heatmap = new AMap.Heatmap(this.mavas.map, this.baseHeatmapOptions);
+    
+    const mean = Util.pluck(this.data, 'count').reduce((sum, number) => {return sum + number}) / this.data.length,
+      std = Math.sqrt(Util.pluck(this.data, 'count').reduce((sum, number) => {return sum + Math.pow(number - mean, 2)}) / this.data.length);
+    
+    this.heatmap.setDataSet({
       data: this.data,
-      max: 1200,
+      max: Math.floor(mean + std * 2),
     });
   };
   
@@ -68,6 +94,33 @@ export default class Heatmap extends React.Component {
     });
     
     return this.data;
+  };
+  
+  mapZoomToRadius(zoomLevel) {
+    const mapping = {
+      '1': 1,
+      '2': 1,
+      '3': 1,
+      '4': 1,
+      '5': 1,
+      '6': 1,
+      '7': 1,
+      '8': 1,
+      '9': 2,
+      '10': 3,
+      '11': 4,
+      '12': 7,
+      '13': 10,
+      '14': 12,
+      '15': 14,
+      '16': 16,
+      '17': 18,
+      '18': 20,
+      '19': 22,
+      '20': 24,
+    };
+    
+    return mapping[zoomLevel];
   };
   
   render() {
